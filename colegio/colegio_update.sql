4ce242fa04b8a78ceadcfc076586dbf4de35d4e3
Pietro romano <pietronromano@live.com>
2025-10-16 15:18:25 +0200

    update RETURNING

--- colegio/colegio_update.sql
+++ colegio/colegio_update.sql
@@ -4,6 +4,7 @@ SET search_path = colegio;
 UPDATE alumnos SET apellidos = 'Romero Blanco' WHERE dni = '76983214G';
 SELECT * FROM alumnos WHERE dni = '76983214G';
 
-UPDATE matriculas SET fecha_matricula = '2025-11-01' WHERE id_matricula = 1;
-SELECT * FROM matriculas WHERE id_matricula = 1;
+-- Usar "RETURNING" para obtener los nuevos valores automáticamente
+UPDATE matriculas SET fecha_matricula = '2025-11-01' WHERE id_matricula = 1
+RETURNING id_matricula, fecha_matricula;
 
